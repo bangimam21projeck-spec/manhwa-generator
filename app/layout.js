67db2081d470bd2d599d8743cd08e0d0a4247d88
bangimam21projeck-spec <bangimam21projeck@gmail.com>
2026-06-18 21:42:55 +0700

    Menambahkan layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Manhwa Generator - AI Comic Creator',
+  description: 'Generate manhwa komik dengan kecerdasan buatan',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
